3575e9a5bb8b6e3617076d614ea7bc08331a8022
tofutych <martirosov.davidka@mail.ru>
2025-05-10 01:18:26 +0300

    refactor: expose additional classes in __init__.py for cleaner imports

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -1,10 +1,19 @@
 from .csv_reader import CsvReader
-from .report_builder import BuilderFactory, ReportBuilder
+from .report_builder import (
+    BuilderFactory,
+    DepartmentReport,
+    Employee,
+    PayoutBuilder,
+    ReportBuilder,
+)
 from .report_writer import WriterFactory
 
 __all__ = [
     "CsvReader",
     "BuilderFactory",
+    "DepartmentReport",
+    "Employee",
+    "PayoutBuilder",
     "ReportBuilder",
     "WriterFactory",
 ]
